c58c44dcdf49aa37a2cedfced04e41be8f3601bb
kimsunmee <sunmee.geem@navercorp.com>
2018-04-19 18:09:20 +0900

    lazyloading적용

--- build/app.js
+++ build/app.js
@@ -1,31 +1,32 @@
+'use strict';
+
 // const add = (a,b) => a+b;
 //alert('test')
 // import Swiper from 'swiper';
 var swiper = new Swiper('.swiper-container', {
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination'
     },
     preloadImages: false,
-    lazyLoading: true,
+    lazyLoading: true
 });
-const langSelect = () => {
-    const lang = window.navigator.language;
+var langSelect = function langSelect() {
+    var lang = window.navigator.language;
     var el = document.querySelector('.langSelectWrap');
-    if(lang !== 'ko') {
+    if (lang !== 'ko') {
         document.querySelector('.langSelectWrap').style.display = 'none';
     }
-}
+};
 
-const scrollEvent = () => {
+var scrollEvent = function scrollEvent() {
     // var scrollValue = window.scrollY; //scrollY는 크롬만됨
-    const scrollValue = () => {
+    var scrollValue = function scrollValue() {
         var el = document.querySelector('.linkToMoveWrap');
-        
-        if(window.pageYOffset >= 100) el.classList.add('shown');
-        else el.classList.remove('shown');
-    }
+
+        if (window.pageYOffset >= 100) el.classList.add('shown');else el.classList.remove('shown');
+    };
     window.addEventListener('scroll', scrollValue, false);
     document.removeEventListener('scroll', scrollValue, false);
-}
+};
 langSelect();
 scrollEvent();
